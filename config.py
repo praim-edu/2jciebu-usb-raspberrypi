98973af3ffc30d820aeb18b92dbb50a4ade3241c
Nicola Manica <nicola.manica@praim.com>
2019-07-18 11:07:52 +0200

    Remove token id

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 USERID="" # user mail
 APITOKEN="" # Databoom api token
-CLIENTID="y1Bk-0ukkus6pz8" # es. XXXX-YYYYYYYY
-DEVICEID="0ukkus6pz8" # es. YYYYYYYYY
+CLIENTID="" # es. XXXX-YYYYYYYY
+DEVICEID="" # es. YYYYYYYYY
 
